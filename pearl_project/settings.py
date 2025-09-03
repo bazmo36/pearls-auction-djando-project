916558f298d36a367902a6cb266f2b1c41ac41dd
bazmo36 <mnalqallaf362@gmail.com>
2025-09-03 14:50:41 +0300

    fix the image bug

--- pearl_project/settings.py
+++ pearl_project/settings.py
@@ -135,3 +135,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_URL = "login"
 LOGIN_REDIRECT_URL = "profile"       
 LOGOUT_REDIRECT_URL = "profile"
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
